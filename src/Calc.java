bdaf080170ca71c74357962bc650fca3430aa244
VLIGS <liudmila.veshneva@gmail.com>
2015-12-31 09:57:32 +0000

    empty string test passed Calc kata

--- src/Calc.java
+++ src/Calc.java
@@ -1,7 +1,17 @@
-public class Calc {
+import java.util.Stack;
 
+public class Calc {
     public double evaluate(String expr) {
-        // TODO: Your awesome code here
+        if(expr.length()==0){
+            return 0;
+        }
+        String [] myExpressionString = expr.split("[\\s]+");
+        Stack<String> myExpressionStack = new Stack<String>();
+        for(int i = 0; i< myExpressionString.length; i++){
+            if(!myExpressionString[i].equals("+")&&!myExpressionString[i].equals("+")&&!myExpressionString[i].equals("+")&&!myExpressionString[i].equals("+")){
+
+            }
+        }
         return 0;
     }
 }
