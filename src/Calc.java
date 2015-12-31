04cbf4f89db72924e95c825fd37db30f2923716c
VLIGS <liudmila.veshneva@gmail.com>
2015-12-31 13:43:07 +0000

    all tests but simple case passed Calc kata

--- src/Calc.java
+++ src/Calc.java
@@ -1,4 +1,5 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calc {
     public double evaluate(String expr) {
@@ -6,18 +7,37 @@ public class Calc {
             return 0;
         }
         String [] myExpressionString = expr.split("[\\s]+");
-        Stack<String> myExpressionStack = new Stack<String>();
+        List<String> myExpressionStack = new ArrayList<String>();
         double myResult = 0.0;
         for(int i = 0; i< myExpressionString.length; i++){
             if(!myExpressionString[i].equals("+")&&!myExpressionString[i].equals("-")&&!myExpressionString[i].equals("*")&&!myExpressionString[i].equals("/")){
-                myExpressionStack.push(myExpressionString[i]);
+                myExpressionStack.add(myExpressionString[i]);
+            }
+            else{
+                myResult = Double.parseDouble(myExpressionStack.get(0));
+                for(int k=1; k<myExpressionStack.size();i++){
+                    if(myExpressionString[i].equals("+")){
+                        myResult = myResult + Double.parseDouble(myExpressionStack.get(k));
+                    }
+                    else if(myExpressionString[i].equals("-")){
+                        myResult = myResult - Double.parseDouble(myExpressionStack.get(k));
+                    }
+                    else if(myExpressionString[i].equals("*")){
+                        myResult = myResult * Double.parseDouble(myExpressionStack.get(k));
+                    }
+                    else if(myExpressionString[i].equals("/")){
+                        myResult = myResult / Double.parseDouble(myExpressionStack.get(k));
+                    }
+                    myExpressionStack = new ArrayList<String>();
+                    myExpressionStack.add(""+myResult+"");
+                }
             }
         }
-        if(myExpressionStack.empty()){
-            return myResult;
+        if(myExpressionStack.size()!=0){
+            return Double.parseDouble(myExpressionStack.get(myExpressionStack.size()-1));
         }
         else{
-            return Double.parseDouble(myExpressionStack.pop());
+            return myResult;
         }
     }
 }
