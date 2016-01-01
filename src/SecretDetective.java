46391ac60589ec12ed3c23d16ecc127debadf4cb
VLIGS <liudmila.veshneva@gmail.com>
2016-01-01 15:41:12 +0000

    created inner class to help with implementation

--- src/SecretDetective.java
+++ src/SecretDetective.java
@@ -1,7 +1,25 @@
+import java.util.List;
+
 public class SecretDetective {
 
     public String recoverSecret(char[][] triplets) {
         return  null;
     }
 
+    public class lettersCluster{
+        private String myCenter;
+        private List<String> myLeaves;
+        public lettersCluster(String myString){
+            myCenter = myString;
+        }
+        public void addLeave(String myLeave){
+            myLeaves.add(myLeave);
+        }
+        public void removeLeave(String myLeave){
+            myLeaves.remove(myLeaves.indexOf(myLeave));
+        }
+        public String getLeave(String myLeave){
+            return myLeaves.get(myLeaves.indexOf(myLeave));
+        }
+    }
 }
