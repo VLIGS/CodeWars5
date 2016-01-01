0fefadcb2e1f59f5155e74674123c54070ebbfc5
VLIGS <liudmila.veshneva@gmail.com>
2016-01-01 18:33:19 +0000

    started on train implementation of SecretDetective kata

--- src/SecretDetective.java
+++ src/SecretDetective.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecretDetective {
-
+    private List<lettersCluster> myLetters = new ArrayList<lettersCluster>();
     public String recoverSecret(char[][] triplets) {
+        for(int i =0; i<triplets.length; i++){
+            train(triplets[i]);
+        }
         return  null;
+    }
+    private void train(char [] myCharArray){
+        for (int i = 0; i <myCharArray.length; i++){
+            if (!containsLetter(Character.toString(myCharArray[i]))) {
+                lettersCluster myLettersCluster = new lettersCluster(Character.toString(myCharArray[i]));
+                myLettersCluster.addLeave(Character.toString(myCharArray[i+1]));
+            }
+            else{
+                //add leave
+            }
+        }
+    }
+    private boolean containsLetter (String myLetter){
+        return true;
     }
 
     public class lettersCluster{
@@ -20,6 +38,17 @@ public class SecretDetective {
         }
         public String getLeave(String myLeave){
             return myLeaves.get(myLeaves.indexOf(myLeave));
+        }
+        public String getCentre(){
+            return myCenter;
+        }
+        public boolean isEmpty(){
+            if(myLeaves.size()==0){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
     }
 }
