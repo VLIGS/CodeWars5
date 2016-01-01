ec40af04894eab1cf04c49eacafb8e72fd7167dc
VLIGS <liudmila.veshneva@gmail.com>
2016-01-01 19:41:11 +0000

    finished train implementation of SecretDetective kata

--- src/SecretDetective.java
+++ src/SecretDetective.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SecretDetective {
-    private List<lettersCluster> myLetters = new ArrayList<lettersCluster>();
+    public List<lettersCluster> myLetters = new ArrayList<lettersCluster>();
     public String recoverSecret(char[][] triplets) {
         for(int i =0; i<triplets.length; i++){
             train(triplets[i]);
@@ -10,18 +10,37 @@ public class SecretDetective {
         return  null;
     }
     private void train(char [] myCharArray){
-        for (int i = 0; i <myCharArray.length; i++){
+        for (int i = 0; i <myCharArray.length-1; i++){
             if (!containsLetter(Character.toString(myCharArray[i]))) {
                 lettersCluster myLettersCluster = new lettersCluster(Character.toString(myCharArray[i]));
                 myLettersCluster.addLeave(Character.toString(myCharArray[i+1]));
+                myLetters.add(myLettersCluster);
             }
             else{
-                //add leave
+                addClusterLeave(Character.toString(myCharArray[i]),Character.toString(myCharArray[i+1]));
             }
+        }
+        if(!containsLetter(Character.toString(myCharArray[2]))){
+            lettersCluster myLettersCluster = new lettersCluster(Character.toString(myCharArray[2]));
+            myLetters.add(myLettersCluster);
         }
     }
     private boolean containsLetter (String myLetter){
-        return true;
+        for(lettersCluster myItem: myLetters){
+            if(myItem.getCentre().equals(myLetter)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private void addClusterLeave (String myCentre, String myLeave){
+        for(lettersCluster myItem: myLetters){
+            if(myItem.getCentre().equals(myCentre)){
+                if(!myItem.containsLeave(myLeave)){
+                    myItem.addLeave(myLeave);
+                }
+            }
+        }
     }
 
     public class lettersCluster{
@@ -29,6 +48,7 @@ public class SecretDetective {
         private List<String> myLeaves;
         public lettersCluster(String myString){
             myCenter = myString;
+            myLeaves = new ArrayList<String>();
         }
         public void addLeave(String myLeave){
             myLeaves.add(myLeave);
@@ -41,6 +61,12 @@ public class SecretDetective {
         }
         public String getCentre(){
             return myCenter;
+        }
+        public boolean containsLeave(String myLeave){
+            if(myLeaves.contains(myLeave)){
+                return true;
+            }
+            return false;
         }
         public boolean isEmpty(){
             if(myLeaves.size()==0){
