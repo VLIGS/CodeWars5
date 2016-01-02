17a21fefb92e510c10288ecd7664ce4897d9fcec
VLIGS <liudmila.veshneva@gmail.com>
2016-01-02 10:42:48 +0000

    progress on implementation of SecretDetective kata

--- src/SecretDetective.java
+++ src/SecretDetective.java
@@ -4,9 +4,15 @@ import java.util.List;
 public class SecretDetective {
     public List<lettersCluster> myLetters = new ArrayList<lettersCluster>();
     public String recoverSecret(char[][] triplets) {
+        List <String> mySecrectString = new ArrayList<String>();
+        String myCurrentString = "";
         for(int i =0; i<triplets.length; i++){
             train(triplets[i]);
         }
+        //find letter with no leaves and add it to secret string
+        mySecrectString.add(findLetterNoLeaves());
+        myCurrentString = mySecrectString.get(0);
+        deleteCenter(myCurrentString);
         return  null;
     }
     private void train(char [] myCharArray){
@@ -24,6 +30,22 @@ public class SecretDetective {
             lettersCluster myLettersCluster = new lettersCluster(Character.toString(myCharArray[2]));
             myLetters.add(myLettersCluster);
         }
+    }
+    private void deleteCenter(String stringToDelete){
+        for(lettersCluster myItem: myLetters){
+            if(myItem.getCentre().equals(stringToDelete)){
+               myLetters.remove(myItem);
+                return;
+            }
+        }
+    }
+    private String findLetterNoLeaves(){
+        for(lettersCluster myItem: myLetters){
+            if(myItem.isEmpty()){
+                return myItem.getCentre();
+            }
+        }
+        return null;
     }
     private boolean containsLetter (String myLetter){
         for(lettersCluster myItem: myLetters){
@@ -62,6 +84,7 @@ public class SecretDetective {
         public String getCentre(){
             return myCenter;
         }
+
         public boolean containsLeave(String myLeave){
             if(myLeaves.contains(myLeave)){
                 return true;
