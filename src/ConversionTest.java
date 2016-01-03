c1e1d728388248f2482d31b56bf3122a720a5d88
VLIGS <liudmila.veshneva@gmail.com>
2016-01-03 10:11:51 +0000

    added more tests, Conversion kata

--- src/ConversionTest.java
+++ src/ConversionTest.java
@@ -6,10 +6,17 @@ public class ConversionTest {
     private Conversion conversion = new Conversion();
 
     @Test
-    public void shouldCovertToRoman() {
+    public void shouldCovertToRomanSimple() {
         assertEquals("solution(1) should equal to I", "I", conversion.solution(1));
         assertEquals("solution(4) should equal to IV", "IV", conversion.solution(4));
         //assertEquals("solution(6) should equal to VI", "VI", conversion.solution(6));
+    }
+    @Test
+    public void shouldCovertToRomanHarder() {
+        assertEquals("solution(1000) should equal to M", "M", conversion.solution(1000));
+        assertEquals("solution(1990) should equal to MCMXC", "MCMXC", conversion.solution(1990));
+        assertEquals("solution(2008) should equal to MMVIII", "MMVIII", conversion.solution(2008));
+        assertEquals("solution(1666) should equal to MDCLXVI", "MDCLXVI", conversion.solution(1666));
     }
 
 }
